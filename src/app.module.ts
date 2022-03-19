08b452b95b944170d3153dc3bce0129f9557e4c4
BakPeter <bak.pet@gmail.com>
2022-03-19 07:53:32 +0200

    section 9: Configuration management

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { TasksModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TasksModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,25 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './transform.interceptor';
 
+console.log(process.env.npm_config_STAGE);
+console.log(process.env.STAGE);
+console.log(process.env.NODE);
+
 async function bootstrap() {
-  const logger = new Logger('main.ts', { timestamp: true });
-  const app = await NestFactory.create(AppModule);
-
-  app.useGlobalPipes(new ValidationPipe());
-  app.useGlobalInterceptors(new TransformInterceptor());
-
-  const port = 3000;
-  await app.listen(port);
-
-  logger.log(port);
+  // const logger = new Logger('main.ts', { timestamp: true });
+  // const app = await NestFactory.create(AppModule);
+  // // const configService = app.get(ConfigService);
+  // // console.log(configService.get('NODE'));
+  // // console.log(configService.get('ENV_VER_1'));
+  // // console.log(configService.get('ENV_VER_2'));
+  // // console.log(configService.get('ENV_VER_3'));
+  // app.useGlobalPipes(new ValidationPipe());
+  // app.useGlobalInterceptors(new TransformInterceptor());
+  // const port = 3000;
+  // await app.listen(port);
+  // logger.log(port);
 }
 bootstrap();
